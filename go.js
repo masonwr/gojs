2d30e1fd5d414c014abbc577ebe76e25d02a43e1
Winston Riley <masonwr@mac.com>
2015-05-31 16:01:16 -0600

    adds notes

--- go.js
+++ go.js
@@ -52,6 +52,8 @@ for (var i = 0; i < 9; i++){
   context.fillCircle(x, y, 5, LINE_COLOR);  
 }
 
+
+
 $("#canvas").click(function(event){
   var x = event.offsetX;
   var y = event.offsetY;
@@ -90,3 +92,29 @@ function printBoard () {
     console.log(board[i].toString());
   }
 }
+
+
+/* refactoring ideas
+ *
+ * build out the board objct a bit more.
+ *
+ * make the interface such that the registering the clicking is abstracted
+ * away from the gui.
+ *
+ * so the interface would look like this:
+ * ` board.layStone(x, y, currentPlayer);
+ *
+ * something like that.
+ *
+ * the game object need to then also be built out.
+ *
+ * also the board could have a reference to a collection of 
+ * stones. and after the clicking had happened the stones could be redrawn.
+ * this way we can possible make the idea of groups and such? may be.
+ *
+ * what do i need to work on first:
+ *  1) general refactoring
+ *  2) build out objects, makge game object, make board object. make interfaces.
+ *  3) add player switching
+ *  4) try to remove dead stones
+ */
