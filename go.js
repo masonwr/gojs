724c34eda35841d38a1c9fcc48cdb875cf26aab4
Winston Riley <masonwr@mac.com>
2015-06-06 16:47:15 -0600

    WIP getting roll back functionality to work

--- go.js
+++ go.js
@@ -14,7 +14,7 @@ var SIZE = 950;
 var LINE_WIDTH = 2;
 var GAME_SIZE = BOARDS.medium;
 var LINE_COLOR = '#333333';
-var offset = SIZE / GAME_SIZE;
+var OFFSET = SIZE / GAME_SIZE;
 
 // appending some utility methods
 CanvasRenderingContext2D.prototype.fillCircle = function (x, y, r, color) {
@@ -38,11 +38,11 @@ context.fillStyle = LINE_COLOR;
 
 // draw board grid
 for (var i = 0; i < GAME_SIZE; i++) {
-    var line_len = SIZE - offset;
-    var x = (i * offset) + offset;
+    var line_len = SIZE - OFFSET;
+    var x = (i * OFFSET) + OFFSET;
 
-    context.fillRect(x, offset, LINE_WIDTH, line_len);
-    context.fillRect(offset, x, line_len, LINE_WIDTH);
+    context.fillRect(x, OFFSET, LINE_WIDTH, line_len);
+    context.fillRect(OFFSET, x, line_len, LINE_WIDTH);
 }
 
 // draw star points
@@ -54,8 +54,8 @@ for (var i = 0; i < 9; i++) {
     var div = f(Math.floor(i / 3));
     var rem = f(i % 3);
 
-    var x = div * offset + LINE_WIDTH / 2;
-    var y = rem * offset + LINE_WIDTH / 2;
+    var x = div * OFFSET + LINE_WIDTH / 2;
+    var y = rem * OFFSET + LINE_WIDTH / 2;
 
     context.fillCircle(x, y, 5, LINE_COLOR);
 }
@@ -77,8 +77,8 @@ $("#forground").click(function (event) {
     var x = event.pageX;
     var y = event.pageY;
 
-    var xp = Math.round(x / offset);
-    var yp = Math.round(y / offset);
+    var xp = Math.round(x / OFFSET);
+    var yp = Math.round(y / OFFSET);
 
     if (board.isEmpty(xp, yp)) {
         if (board.layStone(xp, yp, players[currentPlayer])) {
@@ -89,8 +89,20 @@ $("#forground").click(function (event) {
     }
 
     context_forground.clearRect(0, 0, canvas.width, canvas.height); // cleans the forground first
-    drawStones(board, context_forground, offset);
+    drawStones(board, context_forground, OFFSET);
 });
+
+document.onkeypress = function(e) {
+    if (boardStates.length > 1) {
+        e = e || window.event;
+        var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
+        if (charCode == 122) {
+            board.stones = _.cloneDeep(boardStates.pop());
+            context_forground.clearRect(0, 0, canvas.width, canvas.height); // cleans the forground first
+            drawStones(board, context_forground, OFFSET);
+        }
+    }
+};
 
 // HELPERS
 function drawStones(board, context_forground, offset) {
