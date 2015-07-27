e3d12734be630f28ce22dbd2be76f132dd1a39dc
Winston <masonwr@mac.com>
2015-07-26 20:47:48 -0600

    still trying to get login working

--- gojs/loginPage.js
+++ gojs/loginPage.js
@@ -1,10 +1,17 @@
 if (Meteor.isClient) {
 
     Template.loginPage.onRendered( function () {
-    
 
         $('#term').terminal(function(command, term) {
             console.log("are we getting here?");
+
+            if (command == 'playgo') {
+                term.echo("please enter desured user name: ");
+                //console.log("term", term);
+                //term.exit();
+                return;
+            }
+
             if (command == 'test') {
                 term.echo("you just typed 'test'");
             } else {
@@ -12,12 +19,18 @@ if (Meteor.isClient) {
             }
         }, { login: function(user, pass, call){
 
+            if (user == 'playgo') {
+                call(true);
+                return;
+            }
+
             Meteor.loginWithPassword(user, pass, function(err, thing){
-                console.log("what is all this");
-                debugger;
+                if (err) call(false);
             });
 
-        } });
+        },
+        greetings: "please enter email"
+        })
 
     })
 
