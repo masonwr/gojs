cc25fe4067aa1eaca1ecfe9d7bb423842cb5705d
Winston <masonwr@mac.com>
2015-08-11 23:58:36 -0600

    WIP singup fomr change to modal sign up

--- gojs/Views/Login.es6.js
+++ gojs/Views/Login.es6.js
@@ -1,6 +1,32 @@
 if (Meteor.isClient) {
 
+    Template.Login.onCreated( () => {
+        let showSignup = window.location.href.split('/').pop() == 'add'; 
+        Session.set(SHOW_SIGNUP, showSignup);
+    });
+
     Template.Login.events({
+        'click #signup-icon': () => {
+            vex.dialog.open({
+                message: 'Register your username and password:',
+                input:`<input name="username" type="text" placeholder="Username" required />
+                       <input name="password" type="password" placeholder="Password" required />`,
+                buttons: [
+                    $.extend({}, vex.dialog.buttons.YES, {
+                        text: 'Login'
+                    }), $.extend({}, vex.dialog.buttons.NO, {
+                        text: 'Back'
+                    })
+                ],
+
+                callback: function(data) {
+                    if (data === false) return;
+                    //Accounts.createUser(data);
+                    Meteor.call('registerUser', data);
+                }
+            });
+        },
+
         'click #login-icon': () => {
             vex.dialog.open({
                 message: 'Enter your username and password:',
--- gojs/server/accounts.es6.js
+++ gojs/server/accounts.es6.js
@@ -1,24 +1,18 @@
-Accounts.onCreateUser((options, user) => {
-    // add any additional logic to creating a user
-    // like adding icon ...
-    return user;
+
+Accounts.config({
+      forbidClientAccountCreation : true
 });
 
-WebApp.connectHandlers.use("/add", function(req, res, next) {
-    console.log("req: ", req.query);
-    
-    try{
-        check(req.query, { username: String
-                         , password: String
-                         //, token: String
-        });
-    } catch (err){
-        res.writeHead(200);
-        res.end("there has been an error");
+
+Meteor.methods({
+
+    registerUser({username, password, token}){
+        console.log("createuser called");
+        console.log(username);
+        console.log(password);
+        console.log(token);
     }
-    
-    Accounts.createUser(req.query);
 
-    res.writeHead(200);
-    res.end(JSON.stringify(req.query));
 });
+
+
